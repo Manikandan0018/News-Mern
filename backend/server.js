3342d692c4cd6515f0441d6fd9407e82c7807a20
manikandan <manikandan110305@gmail.com>
2025-11-16 00:19:58 +0530

    update: improved Login UI/UX and removed bg image

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,20 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, 
+    methods: ["GET"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 
 // Routes
 app.use("/api/news", newsRoutes);
 
+// Server Start
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Backend running on port ${PORT}`));
--- frontend/src/api/newsApi.js
+++ frontend/src/api/newsApi.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
+const API_BASE = import.meta.env.PROD
+  ? "https://news-rpek.onrender.com/api/news" // backend URL on Render
+  : "/api/news"; 
+
 export const fetchNews = async (params) => {
-  const res = await axios.get("/api/news", { params });
+  const res = await axios.get(API_BASE, { params });
   return res.data;
 };
